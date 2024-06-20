19b188685001b4c50ce084aef1f634aa7dd24f8f
uzair-hijazi <suikh23@gmail.com>
2024-06-21 02:31:06 +0500

    Quiz by Uzair Hijazi

---
+++ index.ts
@@ -0,0 +1,56 @@
+#! /usr/bin/env node
+
+import chalk from "chalk";
+import inquirer from "inquirer";
+
+const apilink: string =
+  "https://opentdb.com/api.php?amount=20&category=9&difficulty=medium&type=multiple";
+
+let fetchData = async (data: string) => {
+  let fetchQuiz: any = await fetch(data);
+  let res = await fetchQuiz.json();
+  return res.results;
+};
+let data = await fetchData(apilink);
+
+let startQuiz = async () => {
+  let score: number = 0;
+
+  let name = await inquirer.prompt({
+    name: "candidate",
+    type: "input",
+    message: "What is your name?",
+  });
+  for (let i = 1; i < 20; i++) {
+    let answers = [...data[i].incorrect_answers, data[i].correct_answer];
+    let ans = await inquirer.prompt({
+      name: "quiz",
+      type: "list",
+      message: data[i].question,
+      choices: answers.map((val: any) => val),
+    });
+    if (ans.quiz == data[i].correct_answer) {
+      ++score;
+    }
+  }
+
+  if (score <= 5) {
+    console.log(
+      `Dear ${chalk.bgRedBright.black(
+        name.candidate
+      )}. Sorry! You have failed this test. Your score is ${chalk.bold.red(
+        score
+      )}`
+    );
+  } else {
+    console.log(
+      `Dear ${chalk.bgGreen.blackBright(
+        name.candidate
+      )}. Congratulations! You have passed this test. Your score is ${chalk.bold.bgGreenBright.whiteBright(
+        score
+      )}`
+    );
+  }
+};
+
+startQuiz();
